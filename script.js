01d1295816f0d6ff11c89b68433f8efcde7bd819
Ankit-to-crack-ee <109519680+Ankit-to-crack-ee@users.noreply.github.com>
2023-07-19 16:04:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var inp=document.getElementById("tog");
+var icon= document.getElementById("fa-solid fa-moon");
+inp.addEventListener("click",()=>{
+    document.body.classList.toggle("dark-theme");
+ document.body.section.div.i.classList.toggle("fa-thin fa-sun");
+})
+
+// var n=document.getElementsByClassName("mydream");
+// window.addEventListener("scroll",()=>{
+//     document.body.style.classList.toggle(".rota")
